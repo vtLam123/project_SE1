0b9542f6e9fcea55228e13457680d664a4d4f35f
vtLam123 <vtlam20@clc.fitus.edu.vn>
2022-11-11 22:55:49 +0700

    update course

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,6 +4,8 @@ const courseRouter = express.Router();
 
 courseRouter.get('/')
 courseRouter.get('/:id')
+courseRouter.get('/recommend')
+courseRouter.get('/search')
 
 courseRouter.post('/create')
 courseRouter.patch('/update')
