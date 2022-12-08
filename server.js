f8122b2176534cb3a088ca50c63d9a06a64a76d7
vtLam123 <vtlam20@clc.fitus.edu.vn>
2022-12-08 23:54:30 +0700

    connect database

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+const mongoose = require('mongoose');
 const app = require("./app");
-
+const dotenv = require('dotenv');
 const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
     console.log(`App is running on port ${port}...`);
 });
 
-
+dotenv.config({ path: './config.env' });
+mongoose
+.connect(process.env.DATABASE)
+.then(() => {
+    console.log('Connected to DB successfully');
+}).catch(e => {
+    console.log(e);
+});
